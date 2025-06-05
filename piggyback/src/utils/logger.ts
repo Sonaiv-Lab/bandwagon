97b23c73bc0b449f7d038a9ab4e2cb1be2c96c39
lavi <lauviah0622@gmail.com>
2025-06-06 00:26:41 +0800

    refactor: logger

---
+++ piggyback/src/utils/logger.ts
@@ -0,0 +1,34 @@
+import winston from 'winston';
+import { LoggingWinston } from '@google-cloud/logging-winston';
+import type { Options } from '@google-cloud/logging-winston';
+import env from '#/utils/env';
+import {
+  createConsoleTransport,
+  createFileTransPort,
+  createGCPTransport,
+} from '@bandwagon/utils/logger';
+
+const gcpLoggingConfig: Options = {
+  serviceContext: {
+    service: 'bandwagon/piggyback',
+    // TODO, get version from package.json in build time
+    version: '0.0.1',
+  },
+  prefix: 'bandwagon/piggyback/dev',
+};
+
+const logger = winston.createLogger({
+  transports: [
+    createConsoleTransport(),
+    createFileTransPort('.log/logs'),
+    createGCPTransport(env, gcpLoggingConfig),
+  ],
+});
+
+const LOGGING_TRACE_KEY = LoggingWinston.LOGGING_TRACE_KEY;
+const LOGGING_SPAN_KEY = LoggingWinston.LOGGING_SPAN_KEY;
+const LOGGING_SAMPLED_KEY = LoggingWinston.LOGGING_SAMPLED_KEY;
+
+export { LOGGING_TRACE_KEY, LOGGING_SPAN_KEY, LOGGING_SAMPLED_KEY };
+
+export default logger;
---
+++ utils/logger/transports.ts
@@ -0,0 +1,46 @@
+import winston, { format } from 'winston';
+import { LoggingWinston } from '@google-cloud/logging-winston';
+import type { Options } from '@google-cloud/logging-winston';
+import { getLocalCert } from '@bandwagon/utils/getLocalCert';
+import { type Env } from '@bandwagon/utils/env';
+
+const { combine, timestamp, prettyPrint, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const text = label ? `[${level}:${label}]` : `[${level}]`;
+
+  return `${timestamp} ${text.padEnd(1)}:${message}`;
+});
+
+const textFormat = combine(timestamp(), prettyPrint(), myFormat);
+
+const createConsoleTransport = () => new winston.transports.Console({ format: textFormat })
+
+
+const createFileTransPort = (filename: string = '.log/logs') => {
+  return new winston.transports.File({
+    filename,
+    format: textFormat,
+  });
+}
+
+const createGCPTransport = (env: Env, gcpLoggingConfig: Options) => {
+  const deployEnv = env.DEPLOY_ENV;
+
+  if (deployEnv === 'local') {
+    if (!env.FIRESTORE_CERT_LOCAL) {
+      throw new Error('not local env or missing FIRESTORE_CERT_LOCAL env var');
+    }
+
+    const cert = getLocalCert(env.FIRESTORE_CERT_LOCAL);
+
+    gcpLoggingConfig.credentials = cert;
+    gcpLoggingConfig.projectId = cert.projectId;
+  }
+
+  const loggingWinston = new LoggingWinston(gcpLoggingConfig);
+
+  return loggingWinston;
+};
+
+export { createFileTransPort, createConsoleTransport, createGCPTransport };
