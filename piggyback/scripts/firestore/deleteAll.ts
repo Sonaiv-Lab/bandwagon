19a594bcfba83369b637d887b0c23c0ae7559444
lavi <lauviah0622@gmail.com>
2025-06-06 00:53:26 +0800

    fix: delete scripts

--- piggyback/scripts/firestore/deleteAll.ts
+++ piggyback/scripts/firestore/deleteAll.ts
@@ -1,7 +1,7 @@
 import { getFirestore } from '#/resources/db/firestore';
 
 const run = async () => {
-  const store = getFirestore();
+  const store = await getFirestore();
 
   const ref = store.collection('games');
   const allGames = await ref.get();
