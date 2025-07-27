b9a30c5d1b1c3cf6c32ef58aca6c5e1e299ddb64
lavi <lauviah0622@gmail.com>
2025-07-27 22:49:31 +0800

    feat: use custom appBar widget

--- jumbotron-flutter/lib/features/home/home.dart
+++ jumbotron-flutter/lib/features/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:bandwagon/shared/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -41,13 +42,7 @@ class _HomeState extends State<Home> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+      appBar: CustomAppBar(
         title: Text(widget.title),
       ),
       body: Center(
