b0f3c93e21e02cd261964bd675278fd2b0cae938
lavi <lauviah0622@gmail.com>
2025-07-29 00:38:44 +0800

    feat(jumbotron): build script

---
+++ jumbotron-flutter/scripts/build-android-dev-apk.sh
@@ -0,0 +1,2 @@
+# build android apk 的方法，可以發布到 app distribution 裡面
+flutter build apk --flavor freeDevelopment
---
+++ jumbotron-flutter/scripts/build-ios-pre-run.sh
@@ -0,0 +1,2 @@
+# 要 build ios 的 app 之前要先跑這個，再去 xcode 上面手動發布，這樣參考 flutter 部分的 app 資訊才會正確
+flutter build ipa
