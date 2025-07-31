228672638feb15c058e56bbdc56a7d3c593c411f
lavi <lauviah0622@gmail.com>
2025-08-01 00:48:49 +0800

    fix: remove endpoint not use

--- dugout/src/modules/schedule/index.ts
+++ dugout/src/modules/schedule/index.ts
@@ -1,12 +1,8 @@
 import { Hono } from 'hono';
-import { daily } from './daily';
-import { calendar } from './calendar';
 import { tree } from './tree';
 
 const schedule = new Hono().basePath('/schedule');
 
-schedule.route('/', daily);
-schedule.route('/', calendar);
 schedule.route('/', tree);
 
 export { schedule as schedule };
