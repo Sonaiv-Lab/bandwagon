fabb67fa654f1d379768d441dcb22734847cdece
lavi <lauviah0622@gmail.com>
2025-08-10 22:50:03 +0800

    feat(jumbotron): use in schedule

--- jumbotron-flutter/lib/features/schedule/schedule_screen.dart
+++ jumbotron-flutter/lib/features/schedule/schedule_screen.dart
@@ -10,6 +10,83 @@ import 'package:jiffy/jiffy.dart';
 import './widgets/calendar/calendar.dart';
 import 'widgets/schedule_app_bar.dart';
 
+class SuccessSchedule2 extends StatefulWidget {
+  final List<String> monthsList;
+  final String yearMonth;
+  final Future<void> Function() onRefresh;
+
+  const SuccessSchedule2({
+    super.key,
+    required this.monthsList,
+    required this.yearMonth,
+    required this.onRefresh,
+  });
+
+  @override
+  State<SuccessSchedule2> createState() => _SuccessSchedule2State();
+}
+
+class _SuccessSchedule2State extends State<SuccessSchedule2> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(
+      initialPage: widget.monthsList.indexOf(widget.yearMonth),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<ScrollNotification>(
+      onNotification: (ScrollNotification notification) {
+        if (notification is ScrollEndNotification) {
+          final router = GoRouter.of(context);
+
+          final index = _pageController.page?.toInt() ?? 0;
+          final want = '/schedule/${widget.monthsList[index]}';
+
+          if (router.state.uri.toString() != want) {
+            GoRouter.of(
+              context,
+            ).replace('/schedule/${widget.monthsList[index]}');
+          }
+        }
+
+        return false;
+      },
+      child: PageView.builder(
+        controller: _pageController,
+        itemCount: widget.monthsList.length,
+        itemBuilder: (BuildContext context, int index) {
+          final month = widget.monthsList[index];
+          final currentYearMonth = ScheduleYearMonth(month);
+          final startOfMonth = Jiffy.parseFromDateTime(
+            currentYearMonth.datetime,
+          ).startOf(Unit.month).dateTime;
+          final endOfMonth = Jiffy.parseFromDateTime(
+            currentYearMonth.datetime,
+          ).endOf(Unit.month).dateTime;
+
+          return LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return CustomRefreshWrapper(
+                onRefresh: widget.onRefresh,
+                child: SizedBox(
+                  width: constraints.maxWidth,
+                  height: constraints.maxHeight,
+                  child: Calendar(from: startOfMonth, to: endOfMonth),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
 class Schedule extends HookConsumerWidget {
   const Schedule({super.key, required this.yearMonth});
 
@@ -24,8 +101,6 @@ class Schedule extends HookConsumerWidget {
     return switch (status) {
       QueryStatus.success => (() {
         data!.monthsList;
-        final currentYearMonth = ScheduleYearMonth(yearMonth);
-
         if (!data.monthsList.contains(yearMonth)) {
           // TODO, go to the month with games;
           return Center(
@@ -44,50 +119,13 @@ class Schedule extends HookConsumerWidget {
           );
         }
 
-        void goToIndexMonth(int next) {
-          final currentIndex = data.monthsList.indexOf(yearMonth);
-          final index = currentIndex + next;
-          if (index < 0 || index >= data.monthsList.length) {
-            return;
-          }
 
-          GoRouter.of(context).go('/schedule/${data.monthsList[index]}');
-        }
-
-        final startOfMonth = Jiffy.parseFromDateTime(
-          currentYearMonth.datetime,
-        ).startOf(Unit.month).dateTime;
-        final endOfMonth = Jiffy.parseFromDateTime(
-          currentYearMonth.datetime,
-        ).endOf(Unit.month).dateTime;
-
-        return LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            return CustomRefreshWrapper(
-              onRefresh: () async {
-                await updateScheduleTree();
-                return ref.refresh(scheduleTreeProvider.future);
-              },
-              child: GestureDetector(
-                onHorizontalDragEnd: (detail) {
-                  if (detail.primaryVelocity == null) {
-                    return;
-                  }
-
-                  if (detail.primaryVelocity! > 0) {
-                    goToIndexMonth(-1);
-                  }
-                  if (detail.primaryVelocity! < 0) {
-                    goToIndexMonth(1);
-                  }
-                },
-                child: SizedBox(
-                  width: constraints.maxWidth,
-                  height: constraints.maxHeight,
-                  child: Calendar(from: startOfMonth, to: endOfMonth),
-                ),
-              ),
-            );
+        return SuccessSchedule2(
+          yearMonth: yearMonth,
+          monthsList: data.monthsList,
+          onRefresh: () async {
+            await updateScheduleTree();
+            return ref.refresh(scheduleTreeProvider.future);
           },
         );
       })(),
