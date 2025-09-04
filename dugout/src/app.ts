5b8a69aa006f9fe69bc9203f858f6535a3117ab8
lavi <lauviah0622@gmail.com>
2025-09-05 00:46:54 +0800

    feat: testing lineup ping endpoint

--- dugout/src/app.ts
+++ dugout/src/app.ts
@@ -2,11 +2,22 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import logger from './runtime/logger';
 import { schedule, games } from '#/modules';
+import env from '#/runtime/env';
+import { request } from 'undici';
 
 const app = new Hono();
 
 app.get('/ping', async (c) => {
   return c.text('ping');
+});
+
+app.get('/scout/ping', async (c) => {
+  const baseUrl = env.LINEUP_BASE_URL
+  const res = await request(baseUrl + '/ping');
+
+  const text = await res.body.text()
+
+  return c.text(text);
 });
 
 app.route('/', schedule);
