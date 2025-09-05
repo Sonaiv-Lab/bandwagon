8faf6f4d068e998fa3b48ae2603b76aeec96cdfd
lavi <lauviah0622@gmail.com>
2025-09-05 17:45:36 +0800

    feat(infra): reserved internal ip

--- infra/terra/dev/main.tf
+++ infra/terra/dev/main.tf
@@ -20,6 +20,7 @@ resource "google_compute_firewall" "allow_health_check" {
 
   allow {
     protocol = "tcp"
+    # Here should match with health_check in startup.sh
     ports = ["8888"]
   }
 
@@ -34,7 +35,7 @@ resource "google_compute_firewall" "allow_health_check" {
 # 健康檢查，要有這個 MIG 才會幫你 auto healing
 resource "google_compute_health_check" "http" {
   name                = "spot-mig-hc"
-  description = "Health check via http"
+  description         = "Health check via http"
   check_interval_sec  = 10
   timeout_sec         = 5
   healthy_threshold   = 2
@@ -42,8 +43,8 @@ resource "google_compute_health_check" "http" {
 
   # 這個是聽 /health 這個 path，所以一定要起一個服務 http 服務
   http_health_check {
-    request_path       = "/health"
-    port = 8888
+    request_path = "/health"
+    port         = 8888
   }
 }
 
@@ -80,8 +81,8 @@ resource "google_compute_instance_template" "tpl" {
 
   # 啟動的磁碟
   disk {
-    auto_delete  = true
-    boot         = true
+    auto_delete = true
+    boot        = true
     # arm, ubuntu, 然後不會什麼東西都沒有
     source_image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20250828"
     disk_size_gb = 30
@@ -98,14 +99,12 @@ resource "google_compute_instance_template" "tpl" {
     enable-oslogin = "TRUE"
   }
 
-  # 這個是
-  # metadata_startup_script = file("${path.module}/startup.sh")
   metadata_startup_script = templatefile("${path.module}/scripts/startup.sh.tmpl", {
-    setup_gcs = file("${path.module}/scripts/setup_gcs.sh"),
+    setup_gcs    = file("${path.module}/scripts/setup_gcs.sh"),
     setup_docker = file("${path.module}/scripts/setup_docker.sh"),
-    permission = file("${path.module}/scripts/permission.sh"),
+    permission   = file("${path.module}/scripts/permission.sh"),
     health_check = file("${path.module}/scripts/health_check.sh"),
-    run_service = file("${path.module}/scripts/run_service.sh"),
+    run_service  = file("${path.module}/scripts/run_service.sh"),
   })
 
   # 這個是指整個 terraform 會先建立新的 instance，然後再把舊的關掉 
@@ -115,16 +114,40 @@ resource "google_compute_instance_template" "tpl" {
 }
 
 
-resource "google_compute_region_instance_group_manager" "mig" {
+resource "google_compute_address" "reserved_internal" {
+  name         = "spot-mig-reserved-internal"
+  subnetwork   = "default"
+  address_type = "INTERNAL"
+  region       = var.region
+}
+
+resource "google_compute_per_instance_config" "default" {
+  instance_group_manager = google_compute_instance_group_manager.mig.name
+  zone                   = var.zone
+  name                   = "spot-vm-lineup"
+  preserved_state {
+    internal_ip {
+      interface_name = "nic0"
+      auto_delete    = "NEVER"
+      ip_address {
+        address = google_compute_address.reserved_internal.id
+      }
+    }
+  }
+}
+
+
+resource "google_compute_instance_group_manager" "mig" {
   name               = "spot-regional-mig"
   base_instance_name = "spot-vm"
-  region             = var.region
-  target_size        = 1
+  zone               = var.zone
+
+  target_size = 0 # use stateful config
 
   version {
     instance_template = google_compute_instance_template.tpl.self_link
     name              = "primary"
-  } 
+  }
 
   auto_healing_policies {
     health_check      = google_compute_health_check.http.self_link
@@ -132,13 +155,12 @@ resource "google_compute_region_instance_group_manager" "mig" {
   }
 
   update_policy {
-    type           = "PROACTIVE" # 主動替換
-    minimal_action = "REPLACE"   # 直接重建節點（最乾淨）
-    max_surge_fixed       = 1
-    max_unavailable_fixed = 0
+    type                           = "PROACTIVE" # 主動替換
+    most_disruptive_allowed_action = "REPLACE"
+    minimal_action                 = "REPLACE"  # 直接重建節點（最乾淨）
+    replacement_method             = "RECREATE" # 設定 stateful mig 一定要為 RECREATE
+    max_surge_fixed                = 0          # replace method 是 RECREATE 要是 0
+    max_unavailable_fixed          = 1
   }
 
-  distribution_policy_zones = [
-    "${var.region}-a",
-  ]
 }
