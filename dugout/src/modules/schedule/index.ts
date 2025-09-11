017a6c7bd20a00e6a3bcfd6be03d02eed5cc8caf
lavi <lauviah0622@gmail.com>
2025-09-12 00:18:01 +0800

    feat: remove prev dugout

--- dugout/src/modules/schedule/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono';
-import { tree } from './tree';
-
-const schedule = new Hono().basePath('/schedule');
-
-schedule.route('/', tree);
-
-export { schedule as schedule };
--- dugout/src/runtime/config.ts
+++
@@ -1,8 +0,0 @@
-import packageJSON from '#/package.json';
-
-
-const PROJECT_NAME = packageJSON['project_name'];
-const VERSION = packageJSON['version'];
-const NAME = packageJSON['name'];
-
-export { PROJECT_NAME, VERSION, NAME };
