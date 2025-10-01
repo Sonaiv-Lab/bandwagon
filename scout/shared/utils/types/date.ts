cb392a9ae591d0bea38ac5feb856776b1544a1c8
lavi <lauviah0622@gmail.com>
2025-10-01 15:00:43 +0800

    refactor: date utils

--- scout/shared/utils/types/date.ts
+++ scout/shared/utils/types/date.ts
@@ -1,4 +1,13 @@
+import z from "zod";
+
 export type Year = string; // YYYY
-export type MonthString = string; // YYYY-MM
+export type YYYYMMStr = string; // YYYY-MM
 export type DateYYYY_MM_DD = string; // YYYY-MM-DD
 export type ISODateTimeString = string;
+
+export const yearStrSchema = z.string().regex(/\d\d\d\d/);
+export const monthStrSchema = z.string().refine((input) => {
+  const numMonth = Number(input);
+
+  return !Number.isNaN(numMonth) && numMonth >= 1 && numMonth <= 12
+}, {error: 'invalid month string'});
