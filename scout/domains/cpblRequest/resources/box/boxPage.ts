74627c936f374b428fb7deaa5a2a5ff7eb213b46
lavi <lauviah0622@gmail.com>
2025-10-17 01:36:44 +0800

    feat: use new DI context type

--- scout/domains/cpblRequest/resources/box/boxPage.ts
+++ scout/domains/cpblRequest/resources/box/boxPage.ts
@@ -1,9 +1,7 @@
 import { intercept } from '#resources/fetcher/utils/interceptNetworkFromPage';
 import z from 'zod';
-import { Firestore } from '#shared/external/firestore';
 import { Processor } from 'bullmq';
 import { URLSearchParams } from 'node:url';
-import { UnstableQueue } from '#shared/external/queue/unstableQueue';
 import { stringifyRecord } from '#shared/utils/stringifyRecord';
 import {
   BoxPageParams,
@@ -11,7 +9,7 @@ import {
   boxPageParamsSchema,
 } from './utils';
 import * as getLive from './getLive';
-import type { Applicable } from '#shared/utils/container';
+import type { Applicable, RegisteredContext } from '#shared/utils/container';
 
 const boxPageJobName = `cpbl::box::boxPage`;
 
@@ -25,7 +23,7 @@ const createBoxPageJob = (params: BoxPageParams) => {
   };
 };
 
-export const addBoxPageJob: Applicable = (ctx, props: unknown) => {
+export const addBoxPageJob = (ctx: RegisteredContext, props: unknown) => {
   const result = boxPageParamsSchema.safeParse(props, { reportInput: true });
   if (!result.success) {
     const pretty = z.prettifyError(result.error);
@@ -40,56 +38,10 @@ export const addBoxPageJob: Applicable = (ctx, props: unknown) => {
   return ctx.unstableQueue.queue.add(name, data, opts);
 };
 
-// 這個 processor 的目的是為了建立另外一個 job
-const createBoxPageProcessor: ({
-  getStore,
-  getUnstableQueue,
-}: {
-  getStore: () => Firestore;
-  getUnstableQueue: () => UnstableQueue;
-}) => Processor<BoxPageParams> =
-  ({ getStore, getUnstableQueue }) =>
-  async (job) => {
-    const unstableQueue = getUnstableQueue();
-
-    const params = new URLSearchParams(job.data);
-    const siteUrl = `https://www.cpbl.com.tw/box?${params.toString()}`;
-    const endpoint = `https://www.cpbl.com.tw/box/getlive`;
-
-    const { [endpoint]: { request } = {} } = await intercept(siteUrl, [
-      endpoint,
-    ]);
-
-    if (!request) {
-      throw new Error(`${endpoint} endpoint not found`);
-    }
-
-    const formPayload = request.postData();
-    const payload = Object.fromEntries(new URLSearchParams(formPayload ?? ''));
-
-    const headers = request.headers();
-
-    const validPayload = getlivePayloadSchema.parse(payload);
-
-    const getLiveJob = await getLive.addJob(
-      { unstableQueue },
-      {
-        payload: validPayload,
-        headers,
-      }
-    );
-
-    console.log('getLiveJob', getLiveJob.data);
-
-    return getLiveJob;
-  };
-
 const boxPageProcessor: Applicable<Processor<BoxPageParams>> = async (
-  ctx,
+  ctx: RegisteredContext,
   job
 ) => {
-  const unstableQueue = ctx.unstableQueue;
-
   const params = new URLSearchParams(job.data);
   const siteUrl = `https://www.cpbl.com.tw/box?${params.toString()}`;
   const endpoint = `https://www.cpbl.com.tw/box/getlive`;
@@ -107,22 +59,16 @@ const boxPageProcessor: Applicable<Processor<BoxPageParams>> = async (
 
   const validPayload = getlivePayloadSchema.parse(payload);
 
-  const getLiveJob = await getLive.addJob(
-    { unstableQueue },
-    {
-      payload: validPayload,
-      headers,
-    }
-  );
-
-  console.log('getLiveJob', getLiveJob.data);
+  const getLiveJob = await getLive.addJob(ctx, {
+    payload: validPayload,
+    headers,
+  });
 
   return getLiveJob;
 };
 
 export {
   boxPageJobName as name,
-  createBoxPageProcessor as createProcessor,
   addBoxPageJob as addJob,
   boxPageProcessor as processor
 };
