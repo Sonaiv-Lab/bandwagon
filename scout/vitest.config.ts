e889cbcef0fee20536adf6670c4a0aeabb9722b1
lavi <lauviah0622@gmail.com>
2025-10-20 17:00:42 +0800

    chore: prettifiy

--- scout/vitest.config.ts
+++ scout/vitest.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
